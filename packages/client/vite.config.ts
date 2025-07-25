3e18368407158482fcb288ea7cbb4b1af7f512b0
hard-working-He <2380588373@qq.com>
2025-07-25 21:17:02 +0800

    vite配置

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
+
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
@@ -19,6 +23,25 @@ export default defineConfig({
       '@styles': resolve(__dirname, './src/styles'),
       '@stores': resolve(__dirname, './src/stores'),
       '@utils': resolve(__dirname, './src/utils'),
+    }
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      // @ts-ignore
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0]
+          }
+        }
+      }
     }
   }
 })
